3b1ff0926bbddb531f83076a60e483c5bc583554
Philippe Faist <phfaist@gmail.com>
2018-08-28 13:56:10 -0700

    improvements

--- phf-edit-text.el
+++ phf-edit-text.el
@@ -1,26 +1,34 @@
 
 
+(defun phf-edit-text-fontsize (size)
+  (if (eq window-system 'ns)
+      ;; Mac OS X
+      (nth (- size 1) '(
+                        180 ; size == 1
+                        220 ; size == 2
+                        240 ; size == 3
+                        260 ; size == 4
+                        300 ; size == 5
+                        ))
+    ;; Other systems
+    (nth (- size 1) '(
+                      150 ; size == 1
+                      160 ; size == 2
+                      210 ; size == 3
+                      230 ; size == 4
+                      250 ; size == 5
+                      ))
+    )) ; if/defun
+
 (defun phf-edit-text-faceattrs (size)
   (if (eq window-system 'ns)
       ;; Mac OS X
       (list ':family '"Minion Pro"
-        ':height (nth (- size 1) '(
-                                   180 ; size == 1
-                                   220 ; size == 2
-                                   240 ; size == 3
-                                   260 ; size == 4
-                                   300 ; size == 5
-                                   )))
+        ':height (phf-edit-text-fontsize size))
     ;; Other systems -- use Crimson instead
     (list ':family '"Crimson"
-      ':height (nth (- size 1) '(
-                                 180 ; size == 1
-                                 220 ; size == 2
-                                 240 ; size == 3
-                                 260 ; size == 4
-                                 300 ; size == 5
-                                 ))))
-  )
+      ':height (phf-edit-text-fontsize size))
+    ))
 
 (defun phf-edit-text-vfcw (size)
   (if (eq window-system 'ns)
@@ -45,10 +53,10 @@
 
 
 ;; PhF: shortcuts for text editing.
-(defun phf-edit-text (size)
+(defun phf-edit-text (arg)
   (interactive "P") ; accept a prefix argument for "size"
-  (setq size (if size
-                 (prefix-numeric-value size) ; get prefix value
+  (setq size (if arg
+                 (prefix-numeric-value arg) ; get prefix value
                2)  ; defaults to size=2 if no prefix
         )
   (if (eq size 0)
@@ -78,14 +86,23 @@
 ;; PhF -- in case you prefer another font to edit in
 (defun phf-font-merriweather (arg)
   (interactive "P")
-  (if (eq (prefix-numeric-value arg) 1)
+  (setq size (if arg
+                 (prefix-numeric-value arg) ; get prefix value
+               2)  ; defaults to size=2 if no prefix
+        )
+  (if (eq size 0)
+      ; disable merriweather font (disable buffer-face-mode)
       (progn
         (buffer-face-mode -1)
-        (setq buffer-face-mode-face '(:family "Merriweather" :height 170))
-        (buffer-face-mode)
-        (setq line-spacing 0.5))
+        (setq line-spacing nil)
+        )
+    ; enable merriweather font at requested size
     (progn
         (buffer-face-mode -1)
-        (setq line-spacing nil)
-      ))
-  )
+        (setq buffer-face-mode-face
+              (list ':family '"Merriweather" ':height (phf-edit-text-fontsize size)))
+        (setq visual-fill-column-width (phf-edit-text-vfcw size))
+        (buffer-face-mode)
+        (setq line-spacing 0.5))
+    ) ; if
+  ) ; defun
